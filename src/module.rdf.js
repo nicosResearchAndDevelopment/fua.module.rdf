4ed8b6c18e067b6a4f5c9c2a5d9ade2ca2965045
Simon Petrac <pfoerdie@gmail.com>
2020-10-13 18:23:45 +0200

    very promising experiments -> solutions seem clearer!

--- src/module.rdf.js
+++ src/module.rdf.js
@@ -1,5 +1,9 @@
 const
     { promisify } = require('util'),
+    stream = require('stream'),
+    { Readable, Writable } = stream,
+    pipeline = promisify(stream.pipeline),
+    { fileURLToPath, pathToFileURL } = require('url'),
     { join: joinPath } = require('path'),
     { createReadStream } = fs = require('fs'),
     readFile = promisify(fs.readFile),
@@ -7,9 +11,95 @@ const
     ParserN3 = require('@rdfjs/parser-n3'),
     SHACLValidator = require('rdf-validate-shacl'),
     rdfLib = require('rdflib'),
-    N3 = require('n3');
+    jsonld = require('jsonld'),
+    N3 = require('n3'),
+    fetch = require('node-fetch');
 
-// NOTE currently under development (experimentation phase)
+async function loadGraph (param) {
+    if(typeof param === 'string') {
+        if(/^https?:\/\//.test(param)) {
+            const response = await fetch(param, {
+                method: "GET",
+                headers: {
+                    Accept: "text/turtle"
+                }
+            });
+            if(!response.ok) throw new Error(response.statusText);
+            return loadGraph(response.body);
+        } else if(/^file:\/\//.test(param)) {
+            const fileStream = createReadStream(fileURLToPath(param));
+            return loadGraph(fileStream);
+        } else {
+            throw "TODO -> load ttl document";
+        }
+    } else if(param instanceof Readable) {
+        const quadStream = new N3.StreamParser();
+        quadStream.import(param); // https://github.com/rdfjs/N3.js/blob/master/src/N3StreamParser.js#L33
+
+        const quadStore = new N3.Store({ factory: rdfLib.DataFactory });
+        quadStore.import(quadStream); // https://github.com/rdfjs/N3.js/blob/master/src/N3Store.js#L261
+
+        await new Promise((resolve, reject) => quadStream.once('end', resolve).once('error', reject));
+        return quadStore;
+    } else {
+        throw "TODO";
+    }
+    // -> param is an url/file-url
+    // -> param is a rdf document as string/buffer/readable-stream
+} // loadGraph
+
+(async (/* async-iife */) => {
+
+    const
+        pathSegments = [__dirname, '../test', 'data/my-data.ttl'],
+        filePath = joinPath(...pathSegments),
+        // fileBuffer = await readFile(filePath),
+        // fileStream = createReadStream(filePath),
+        // quadStream = new N3.StreamParser(),
+        // quadStore = new N3.Store({ factory: rdfLib.DataFactory }),
+        // quadStore = await loadGraph(pathToFileURL(filePath).toString()),
+        // quadStore = await loadGraph('http://www.w3.org/1999/02/22-rdf-syntax-ns'),
+        quadStore = await loadGraph('https://www.google.com/'),
+        quadStore2 = rdfLib.graph(/*{rdfFactory: N3.DataFactory}*/)
+    ;
+
+    /** {@link https://github.com/rdfjs/N3.js/blob/master/src/N3StreamParser.js#L33} */
+    // quadStream.import(fileStream);
+    /** {@link https://github.com/rdfjs/N3.js/blob/master/src/N3Store.js#L261} */
+    // quadStore.import(quadStream);
+
+    // await new Promise(resolve => setTimeout(resolve, 3));
+    // await new Promise((resolve, reject) => quadStream.once('end', resolve).once('error', reject));
+
+    // console.log(quadStore);
+    quadStore2.addAll(quadStore.getQuads());
+
+    // quadStore2.addAll(quadStore.getQuads().map(quad => new rdfLib.Statement(
+    //     new rdfLib[quad.subject.termType](quad.subject.value),
+    //     new rdfLib[quad.predicate.termType](quad.predicate.value),
+    //     new rdfLib[quad.object.termType](quad.object.value),
+    //     quad.graph.value ? new rdfLib[quad.graph.termType](quad.graph.value) : undefined
+    // )));
+
+    const strQuads = quadStore.getQuads().map(quad =>
+        `${quad.subject.termType}<${quad.subject.value}> ` +
+        `${quad.predicate.termType}<${quad.predicate.value}> ` +
+        `${quad.object.termType}<${quad.object.value}>`
+    );
+
+    const strQuads2 = quadStore2.statementsMatching().map(quad =>
+        `${quad.subject.termType}<${quad.subject.value}> ` +
+        `${quad.predicate.termType}<${quad.predicate.value}> ` +
+        `${quad.object.termType}<${quad.object.value}>`
+    );
+
+    console.log(`Quads: ${strQuads.length}`);
+    console.log(strQuads.join("\n"));
+    // console.log(strQuads2.join("\n"));
+    console.log(`equal? ${strQuads.every(val => strQuads2.includes(val)) && strQuads2.every(val => strQuads.includes(val))}`);
+    debugger;
+
+})(/* async-iife */).catch(console.error);
 
 /**
  * Loads a dataset of n-quads from a ttl-file.
