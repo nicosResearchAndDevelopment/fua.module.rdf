7aaba603993538d4cbcd921b39b7f6e5c2858895
Simon Petrac <pfoerdie@gmail.com>
2020-10-20 15:23:21 +0200

    added dependencies to the package.json

--- src/module.rdf.js
+++ src/module.rdf.js
@@ -1,19 +1,2 @@
-// const
-//     { promisify } = require('util'),
-//     stream = require('stream'),
-//     { Readable, Writable } = stream,
-//     pipeline = promisify(stream.pipeline),
-//     { fileURLToPath, pathToFileURL } = require('url'),
-//     { join: joinPath } = require('path'),
-//     { createReadStream } = fs = require('fs'),
-//     readFile = promisify(fs.readFile),
-//     rdfExt = require('rdf-ext'),
-//     ParserN3 = require('@rdfjs/parser-n3'),
-//     SHACLValidator = require('rdf-validate-shacl'),
-//     rdfLib = require('rdflib'),
-//     jsonld = require('jsonld'),
-//     N3 = require('n3'),
-//     fetch = require('node-fetch');
-
 exports.Dataset = require('./module.rdf.Dataset.js');
 Object.freeze(exports);
