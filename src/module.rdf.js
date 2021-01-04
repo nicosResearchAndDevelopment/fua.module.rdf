2d0f97ac758e465b731215f4a8c1a1b4012c5490
Simon Petrac <pfoerdie@gmail.com>
2021-01-04 14:33:45 +0100

    added RedisStore and MongoDBStore to index

--- src/module.rdf.js
+++ src/module.rdf.js
@@ -1,3 +1,7 @@
 exports.Dataset = require('./module.rdf.Dataset.js');
+
+exports.RedisStore = require('./module.rdf.RedisStore.js');
+exports.MongoDBStore = require('./module.rdf.MongoDBStore.js');
 exports.Neo4jStore = require('./module.rdf.Neo4jStore.js');
+
 Object.freeze(exports);
