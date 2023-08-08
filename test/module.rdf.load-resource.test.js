7361f94183b87fab86a69d72585c956d8dd74eba
Simon Petrac <pfoerdie@gmail.com>
2023-08-08 13:45:30 +0200

    change from resource.ontology to resource.ontology.core

--- test/module.rdf.load-resource.test.js
+++ test/module.rdf.load-resource.test.js
@@ -9,25 +9,25 @@ const
 
 describe('module.rdf : loadDataFiles', function () {
 
-    test('@nrd/fua.resource.ontology', async function () {
-        const dataFiles = await loadDataFiles(require('@nrd/fua.resource.ontology'), factory);
+    test('@nrd/fua.resource.ontology.core', async function () {
+        const dataFiles = await loadDataFiles(require('@nrd/fua.resource.ontology.core'), factory);
         expect(dataFiles.length).toBeGreaterThan(0);
         const datasets = dataFiles.map(file => file.dataset).filter(val => val);
         expect(datasets.length).toBeGreaterThan(0);
     });
 
-    test('@nrd/fua.resource.ontology/ldp', async function () {
-        const dataFiles = await loadDataFiles(require('@nrd/fua.resource.ontology/ldp'), factory);
-        expect(dataFiles.length).toBeGreaterThan(0);
-        const datasets = dataFiles.map(file => file.dataset).filter(val => val);
-        expect(datasets.length).toBeGreaterThan(0);
-    });
-
-    test('@nrd/fua.resource.ontology/odrl', async function () {
-        const dataFiles = await loadDataFiles(require('@nrd/fua.resource.ontology/odrl'), factory);
-        expect(dataFiles.length).toBeGreaterThan(0);
-        const datasets = dataFiles.map(file => file.dataset).filter(val => val);
-        expect(datasets.length).toBeGreaterThan(0);
-    });
+    // test('@nrd/fua.resource.ontology/ldp', async function () {
+    //     const dataFiles = await loadDataFiles(require('@nrd/fua.resource.ontology/ldp'), factory);
+    //     expect(dataFiles.length).toBeGreaterThan(0);
+    //     const datasets = dataFiles.map(file => file.dataset).filter(val => val);
+    //     expect(datasets.length).toBeGreaterThan(0);
+    // });
+    //
+    // test('@nrd/fua.resource.ontology/odrl', async function () {
+    //     const dataFiles = await loadDataFiles(require('@nrd/fua.resource.ontology/odrl'), factory);
+    //     expect(dataFiles.length).toBeGreaterThan(0);
+    //     const datasets = dataFiles.map(file => file.dataset).filter(val => val);
+    //     expect(datasets.length).toBeGreaterThan(0);
+    // });
 
 });
